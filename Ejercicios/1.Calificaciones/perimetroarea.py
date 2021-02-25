95f2d730777e243f1b129e583cc0f6d6954a4bea
Beatriz <cpbeatrizfrm@gmail.com>
2021-02-25 17:39:09 -0600

    ejercicio2

---
+++ Ejercicios/1.Calificaciones/perimetroarea.py
@@ -0,0 +1,16 @@
+
+ # Entrada de datos
+print ("Ingresa el valor del diametro")
+diametro = int(input())
+
+
+# Operaciones o calculos matematicos
+pi = 3.1416
+r = (diametro/2)
+circunferencia = pi * diametro
+area = pow(r,2) * pi
+
+# Imprimir o mostrar en pantalla
+
+print ("El area es ",  area)
+print ("La circunferencia es ",  circunferencia)
