b1fba7c0cf0a0388f1fed0af97838f09d393fa1f
Beatriz <cpbeatrizfrm@gmail.com>
2021-02-24 18:50:54 -0600

    Condicion

---
+++ Ejercicios/1.Calificaciones/Calificaciones.py
@@ -0,0 +1,22 @@
+ 
+ 
+ # Entrada de datos
+print ("Ingresa tu primer calificacion")
+numero1 = int(input())
+print ("Ingresa tu segunda calificacion")
+numero2 = int(input())
+print ("Ingresa tu tercera calificacion")
+numero3 = int(input())
+
+# Operaciones o calculos matematicos
+suma = numero1 + numero2 + numero3
+promedio = suma / 3
+
+# Imprimir o mostrar en pantalla
+print(f"El promedio = {promedio}")
+
+
+if (promedio <5):
+    print("Reprobado")
+else:
+    print("Aprobado")
