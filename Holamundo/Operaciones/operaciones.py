1b83aef8fc24e40233b5f6490290080f967dc22f
Beatriz <cpbeatrizfrm@gmail.com>
2021-02-24 18:17:11 -0600

    Clase 24 Feb

---
+++ Holamundo/Operaciones/operaciones.py
@@ -0,0 +1,25 @@
+numero1 = 10
+numero2 = 2
+
+print ("Ingresa numero 1")
+numero1 = int(input())
+Print ("Ingresa numero 2")
+numero2 = int(input())
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+division = numero1 / numero2
+multiplicacion = numero1 * numero2
+potencia = pow(numero1, numero2)
+raiz_cuadrada = pow(numero1, 0.5)
+raiz_cubica = pow(numero1, 1/3)
+modulo = numero1 % numero2
+
+
+print(f"La suma = {suma}")
+print(f"La resta = {resta}")
+print(f"La multiplicacion = {multiplicacion}")
+print(f"La division = {division}")
+print(f"La raiz cuadrada = {raiz_cuadrada}")
+print(f"La raiz cubica = {raiz_cubica}")
+print(f"El modulo o residuo = {modulo}")
