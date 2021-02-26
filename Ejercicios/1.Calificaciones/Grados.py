7b4542fb32dd333bb9e550e44b198799ef22cad0
Beatriz <cpbeatrizfrm@gmail.com>
2021-02-25 18:45:58 -0600

    ejercicio5

---
+++ Ejercicios/1.Calificaciones/Grados.py
@@ -0,0 +1,14 @@
+
+ # Entrada de datos
+print ("Ingresa un valor en grados centigrados")
+centigrados = int(input())
+
+
+# Operaciones o calculos matematicos
+fahrenheit =  Fahrenheit = 32 + (9 * centigrados / 5)
+kelvin = (centigrados - 32) * 5 / 9 + 273
+
+# Imprimir o mostrar en pantalla
+print(f"En grados Fahrentheit equivale a = {fahrenheit}")
+print(f"En grados Kelvin equivale a = {kelvin}")
+
