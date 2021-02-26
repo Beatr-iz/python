3c896df6fb4c0879a059134687795710f37f7d31
Beatriz <cpbeatrizfrm@gmail.com>
2021-02-25 19:26:39 -0600

    ejercicio6

---
+++ Ejercicios/1.Calificaciones/Formulageneral.py
@@ -0,0 +1,18 @@
+# Entrada de datos
+print ("Ingresa el valor de a")
+print ("Ingresa el valor de b")
+print ("Ingresa el valor de b")
+
+# inpunt= es para recibir un valor como string y para convertirlo a entero int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Operaciones o calculos matematicos
+x1 = (-b+((b*b)-(4*a*c))**0.5)/(2*a)
+x2 = (-b-((b*b)-(4*a*c))**0.5)/(2*a)
+
+# Imprimir o mostrar en pantalla
+print(f"El valor de x1 es igual a = {x1}")
+print(f"El valor de x2 es igual a = {x2}")
+
